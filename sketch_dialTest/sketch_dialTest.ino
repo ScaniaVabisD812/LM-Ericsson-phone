bf107ed21808d33e4f923e37129790b1733b0f03
ScaniaVabisD812 <arvidn04@live.se>
2025-10-02 22:21:03 +0200

    Reads rotary dial voltage

--- sketch_dialTest/sketch_dialTest.ino
+++ sketch_dialTest/sketch_dialTest.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(2,INPUT);
+  Serial.begin(9600);
+  Serial.print("test");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(digitalRead(2) == HIGH){
+    Serial.print("HIGH");
+  }
 }
